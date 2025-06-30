5f1581a67b8497ffd31b00bd34b678b7b04d3b36
Kristina Maevskaya <maevskayakrisfx@gmail.com>
2025-06-30 11:14:07 +1000

    Extend TextureResolve class with additional texture type support
    - Added support for more texture channels (e.g., roughness, emission, displacement)

--- src/usd_indie_pipe/texture_resolve.py
+++ src/usd_indie_pipe/texture_resolve.py
@@ -12,7 +12,10 @@ class TextureResolve:
 
     tex_conversion: dict[str, str] = field(default_factory=lambda: {
         "base_color": "basecolor",
-        "specular": "specular"
+        "specular": "specular",
+        "emission": "emissive",
+        "displacement": "height",
+        "specular_roughness": "roughness"
     })
     mapping: dict[str, pathlib.Path] = field(default_factory=dict)
 
@@ -22,6 +25,7 @@ class TextureResolve:
 
         tex_folder_abs_path = pathlib.Path(self.tex_folder_path)
         for file in tex_folder_abs_path.iterdir():
+
             file_edit = flatten_input_data(str(file.stem))
             for mlx_text, source_tex in self.tex_conversion.items():
                 texture_edit = flatten_input_data(source_tex)
