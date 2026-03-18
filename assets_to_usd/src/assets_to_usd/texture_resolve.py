04cf0f6f957e4a83c8d62affedc1ea68fdf6bec5
Kristina Maevskaya <maevskayakrisfx@gmail.com>
2026-03-18 11:36:45 +1100

    Fix Path import

--- assets_to_usd/src/assets_to_usd/texture_resolve.py
+++ assets_to_usd/src/assets_to_usd/texture_resolve.py
@@ -1,5 +1,5 @@
-import pathlib
 from dataclasses import dataclass, field
+from pathlib import Path
 
 from assets_to_usd.asset_mapping import flatten_input_data
 
@@ -18,14 +18,14 @@ class TextureResolve:
         "specular_roughness": "roughness",
         "opacity": "opacity"
     })
-    mapping: dict[str, pathlib.Path] = field(default_factory=dict)
+    mapping: dict[str, Path] = field(default_factory=dict)
 
     def parse_texture(self) -> dict:
         # we remove the first few characters because it comes with prefix
         name_space_clean = flatten_input_data(self.namespace)
         name_space_edit = name_space_clean[5:] if len(name_space_clean) > 5 else name_space_clean
 
-        tex_folder_abs_path = pathlib.Path(self.tex_folder_path)
+        tex_folder_abs_path = Path(self.tex_folder_path)
         for file in tex_folder_abs_path.iterdir():
 
             file_edit = flatten_input_data(str(file.stem))
